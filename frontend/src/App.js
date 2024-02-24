60726568c23fc41c4c18e5f728928f5bdfc4d75c
Matheus Cardoso <46722962+MatheusCTorres@users.noreply.github.com>
2024-02-24 13:01:31 +0000

    fix api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Axios from  'axios'
-import {useState, useEffect} from 'react'
+import Axios from 'axios'
+import { useState, useEffect } from 'react'
 import Coin from './components/Coin';
 
 
@@ -10,21 +10,37 @@ function App() {
   const [search, setSearch] = useState("")
 
   useEffect(() => {
-    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
-    .then(response => setListOfCoins(response.data.coins))
+    // Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
+    // .then(response => setListOfCoins(response.data.coins))
+    const options = {
+      method: 'GET',
+      url: 'https://openapiv1.coinstats.app/coins',
+      headers: {
+        accept: 'application/json',
+        'X-API-KEY': 'PdIMiIIkl72QGoysVyiBJRfGmIA8f78CNCA7Nht528U='
+      }
+    };
+
+    Axios
+      .request(options)
+      .then(response => setListOfCoins(response.data.result))
+      .catch(function (error) {
+        console.error(error);
+      });
   })
 
-  const filterCoins = listOfCoins.filter((coin) =>{
+  console.log(listOfCoins)
+  const filterCoins = listOfCoins.filter((coin) => {
     return coin.name.toLowerCase().includes(search.toLowerCase())
   })
   return (
     <div className="App">
       <div className="cryptoHeader">
-        <input type="text" placeholder="Search..."  onChange={(event) => setSearch(event.target.value)} />
+        <input type="text" placeholder="Search..." onChange={(event) => setSearch(event.target.value)} />
       </div>
       <div className="cryptoDisplay">
-        {filterCoins.map((coins) =>{
-          return <Coin name={coins.name} icon={coins.icon} price={coins.price}  symbol={coins.symbol}/>
+        {filterCoins.map((coins) => {
+          return <Coin name={coins.name} icon={coins.icon} price={coins.price} symbol={coins.symbol} />
         })}
       </div>
     </div>
